d4d850ba5c5bba889019d068afec1169bc7a0730
Garrett Lander <garrett.jlander@gmail.com>
2025-06-17 17:04:54 +0200

    made chatId nullish

--- zod/schemas.js
+++ zod/schemas.js
@@ -2,5 +2,6 @@ import { z } from 'zod/v4';
 
 export const userMessageSchema = z.object({
   message: z.string().min(1, 'Must have a message'),
-  chatId: z.string().length(24).optional()
+  // nullish makes it both optional, and allows null as a value
+  chatId: z.string().length(24).nullish()
 });
