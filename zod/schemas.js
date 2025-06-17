fff65c5b0e7335ea32fba8463f72f25a83267cd2
Garrett Lander <garrett.jlander@gmail.com>
2025-06-17 17:03:53 +0200

    set stream default to false

--- zod/schemas.js
+++ zod/schemas.js
@@ -2,6 +2,6 @@ import { z } from 'zod/v4';
 
 export const userMessageSchema = z.object({
   message: z.string().min(1, 'Must have a message'),
-  stream: z.boolean(),
+  stream: z.boolean().default(false),
   chatId: z.string().length(24).nullish()
 });
